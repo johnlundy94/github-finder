591a96e914c4f2f292433c8191fcc94e4e476f37
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-30 09:08:00 -0600

    Cleaned up fetch users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,13 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -27,6 +28,9 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  //Set Loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
