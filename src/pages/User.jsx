69c33ce3bcf93c22d279530f73d48f76d340a389
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-04-04 09:50:33 -0600

    Started work on user profile

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,41 @@
-function user() {
-  return <div>user</div>;
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
+import { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../components/layout/Spinner";
+import GithubContext from "../context/github/GithubContext";
+import { useParams } from "react-router-dom";
+
+function User() {
+  const { getUser, user, loading } = useContext(GithubContext);
+
+  const params = useParams();
+
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
+
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return <div>{user.login}</div>;
 }
 
-export default user;
+export default User;
